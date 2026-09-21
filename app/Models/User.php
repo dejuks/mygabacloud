8880a0c0d72c0aa12ad905c65292fa28504193e2
dejuks <dejuks@gmail.com>
2026-09-22 00:28:09 +0300

    'auth-control'

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,10 +9,10 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-
+use Laravel\Sanctum\HasApiTokens; 
 class User extends Authenticatable implements MustVerifyEmail
 {
-    use HasFactory, Notifiable, SoftDeletes;
+    use HasFactory, Notifiable, SoftDeletes,HasApiTokens;
 
     protected $fillable = [
         'name', 'email', 'password', 'avatar', 'bio', 'paypal_email',
